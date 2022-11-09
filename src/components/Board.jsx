66fa8709c47f03c5b5fbf52f5370e59c969a2a56
romerocam <romerocam@hotmail.com>
2022-11-09 16:41:06 -0300

    deploy ready?

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BoardSquare from "./BoardSquare";
 
-const getXYPosition = (i) => {
-  const x = i % 8;
-  const y = Math.abs(Math.floor(i / 8) - 7);
-  return { x, y };
-};
-const isBlack = (i) => {
-  const { x, y } = getXYPosition(i);
-  return (x + y) % 2 === 1;
-};
-const getPosition = (i) => {
-  const { x, y } = getXYPosition(i);
-  const letter = ["a", "b", "c", "d", "e", "f", "g", "h"][x];
-  return `${letter}${y + 1}`;
-};
+export default function Board({ board, turn }) {
+  const [currBoard, setCurrBoard] = useState([]);
 
-export default function Board({ board }) {
+  useEffect(() => {
+    setCurrBoard(turn === "w" ? board.flat() : board.flat().reverse());
+  }, [board, turn]);
+
+  const getXYPosition = (i) => {
+    const x = turn === "w" ? i % 8 : Math.abs((i % 8) - 7);
+    const y =
+      turn === "w" ? Math.abs(Math.floor(i / 8) - 7) : Math.floor(i / 8);
+    return { x, y };
+  };
+  const isBlack = (i) => {
+    const { x, y } = getXYPosition(i);
+    return (x + y) % 2 === 1;
+  };
+  const getPosition = (i) => {
+    const { x, y } = getXYPosition(i);
+    const letter = ["a", "b", "c", "d", "e", "f", "g", "h"][x];
+    return `${letter}${y + 1}`;
+  };
+
   return (
     <div className="board">
-      {board.flat().map((piece, i) => (
+      {currBoard.map((piece, i) => (
         <div key={i} className="square">
           <BoardSquare
             piece={piece}
