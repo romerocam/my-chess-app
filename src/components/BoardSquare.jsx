fc944fafedf1cbd9c57f4959c94ca11d6681ca39
romerocam <romerocam@hotmail.com>
2022-11-09 13:06:16 -0300

    rendering pieces

--- src/components/BoardSquare.jsx
+++ src/components/BoardSquare.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Square from "./Square";
 import Piece from "./Piece";
+import { useDrop } from "react-dnd";
 
 export default function BoardSquare({ piece, black }) {
+    const [, drop] = useDrop({
+        accept: 'piece',
+        drop: (item) => {
+        //   const [fromPosition] = item.id.split('_')
+        //   handleMove(fromPosition, position)
+        },
+      })
   return (
-    <div className="board-square">
+    <div className="board-square" ref={drop}>
       <Square black={black}>{piece && <Piece piece={piece} />}</Square>
     </div>
   );
