b96a1821f4e49422eff6778cee441553a3304680
romerocam <romerocam@hotmail.com>
2023-04-05 08:46:01 -0300

    fixing moves

--- src/components/Promote.jsx
+++ src/components/Promote.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Square from "./Square";
 import { move } from "../Game";
-// const promotionPieces = ["r", "n", "b", "q"];
+const promotionPieces = ["r", "n", "b", "q"];
 
 export default function Promote({ promotion: { from, to, color } }) {
   return (
