8b39f37c9e2ddc382dfa29ae38e96295bb9c735d
Juan Camilo Romero <101602875+romerocam@users.noreply.github.com>
2023-12-07 10:44:56 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={HTML5Backend} >
       <App />
     </DndProvider>
   </React.StrictMode>
