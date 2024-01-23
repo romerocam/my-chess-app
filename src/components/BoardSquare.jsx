1fe961e26a2685a79df0e2518e2840264e2fcf3e
Juan Camilo Romero <101602875+romerocam@users.noreply.github.com>
2024-01-23 10:11:21 -0300

    Update BoardSquare.jsx

--- src/components/BoardSquare.jsx
+++ src/components/BoardSquare.jsx
@@ -9,7 +9,7 @@ export default function BoardSquare({ piece, black, position }) {
   const [, drop] = useDrop({
     accept: "piece",
     drop: (item) => {
-      const [fromPosition] = item.id.split("_");
+      const [fromPosition] = item.id.split(".");
       handleMove(fromPosition, position);
     },
   });
